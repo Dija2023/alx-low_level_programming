1dc8fb41fb6660e5f013f989e454e230883e985a
Khadija Ouahabi <dija.titif2014@gmail.com>
2023-05-24 09:21:42 -0700

    0x0F.Functions pointers

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -7,11 +7,11 @@ int op_div(int a, int b);
 int op_mod(int a, int b);
 
 /**
- * op_add - return the sum of two num .
- * @a: First number .
- * @b: Second number .
+ * op_add - Returns the sum of two numbers.
+ * @a: The first number.
+ * @b: The second number.
  *
- * Return: Sum of a and b.
+ * Return: The sum of a and b.
  */
 int op_add(int a, int b)
 {
@@ -40,22 +40,22 @@ int op_mul(int a, int b)
 	return (a * b);
 }
 /**
- * op_div - returns division of two num .
- * @a: First number.
- * @b: Second number.
+ * op_div - Returns the division of two numbers.
+ * @a: The first number.
+ * @b: The second number.
  *
- * Return: Quotient of a and b.
+ * Return: The quotient of a and b.
  */
 int op_div(int a, int b)
 {
 	return (a / b);
 }
 /**
- * op_mod - return remainder of division of two num .
- * @a: First number.
- * @b: Second number.
+ * op_mod - Returns the remainder of the division of two numbers.
+ * @a: The first number.
+ * @b: The second number.
  *
- * Return: Remainder of the division of a by b.
+ * Return: The remainder of the division of a by b.
  */
 int op_mod(int a, int b)
 {
